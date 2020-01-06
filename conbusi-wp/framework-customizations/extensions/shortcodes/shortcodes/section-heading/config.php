7e17322dc08639f34554e7f4a8dc97302b4b1d93
95media Web Solutions <info@95media.co.uk>
2020-01-06 11:08:56 +0000

    zip theme files

--- conbusi-wp/framework-customizations/extensions/shortcodes/shortcodes/section-heading/config.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('FW')) die('forbidden');
-
-
-$cfg = array(
-    'page_builder' => array(
-        'title' => __('Section Heading', 'unyson'),
-        'tab'   => __('Content Elements', 'FW')
-    )
-);

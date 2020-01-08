3fe7bd59a33763c23b0b43e4950ce43a589d473e
95media Web Solutions <info@95media.co.uk>
2020-01-08 20:29:20 +0000

    updated the FAQ drag and drop

--- conbusi-wp/conbusi-wp/framework-customizations/extensions/shortcodes/shortcodes/faqs/views/view.php
+++ conbusi-wp/conbusi-wp/framework-customizations/extensions/shortcodes/shortcodes/faqs/views/view.php
@@ -19,25 +19,29 @@ $custom_class = $atts['custom_class'];
                     <div class="col-xl-6 col-lg-6">
                         <div class="faq_ask">
                             <h3><?php echo $faq_header;?></h3>
-                            <?php foreach ($faqs as $key => $faq_vals) { ?>
-                                
+                            
+                             
                                 <div id="accordion">
+                                    <?php $count = 2;?>
+                                    <?php foreach ($faqs as $key => $faq_vals) { ?>
                                         <div class="card">
                                             <div class="card-header" id="headingThree">
                                                 <h5 class="mb-0">
-                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo">
+                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree<?php echo $count?>" aria-expanded="false" aria-controls="collapseTwo">
                                                            <?php echo $faq_vals['faq_question']?>
                                                     </button>
                                                 </h5>
                                             </div>
-                                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion" style="">
+                                            <div id="collapseThree<?php echo $count?>" class="collapse" aria-labelledby="headingThree" data-parent="#accordion" style="">
                                                 <div class="card-body"> <?php echo $faq_vals['faq_answer']?>
                                                   <?php echo $faq_vals['faq_answer']?>
                                                 </div>
                                             </div>
                                         </div>
+                                          <?php $count++;  }  ?>
                                     </div>
-                                    <?php } ?>
+                                   
+                                  
                         </div>
                     </div>
                 </div>
